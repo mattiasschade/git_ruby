0bdebfe2c1b5f6f5b14d4074cd996436a6ee9d30
Mattias Schade <mattiasschade@gmail.com>
2023-03-20 15:46:05 -0500

    added average function

--- git_ruby_file.rb
+++ git_ruby_file.rb
@@ -1,12 +1,16 @@
 array = [2,2,10,4,5]
 
-sum = 0
+sum = 0.0
 product = 1
+average = 1
 array.each do |number|
   p number
   sum += number
   product *= number
 end
 
+average = sum / array.length
+
 p sum
 p product
+p average 
